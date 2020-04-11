392b25f2e804f3cdfcac1517602f9dea08a9d109
kraftwerk28 <kefirchik3@gmail.com>
2020-04-11 12:04:10 +0300

    add: import error handling

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+# Jordan-Gauss resolve method
+# Usage: ./main.py input.txt output.txt
+# Input file must contain matrix w/ whitespace-separated numbers
+# and line-break separated rows
+
 import re
 import sys
-from termcolor import colored
-from fractions import Fraction as Fr
-from tabulate import tabulate
-
-# M = input('Matrix width: ')
-# N = input('Matrix height: ')
+try:
+    from fractions import Fraction as Fr
+    from tabulate import tabulate
+except ImportError as e:
+    print('Required dependencies: `fractions`, `tabulate`.')
+    sys.exit(1)
 
 
 def step(mtx, pivot):
@@ -14,7 +19,7 @@ def step(mtx, pivot):
     res = [[None for _ in range(M)] for _ in range(N)]
     px, py = pivot
     pv = mtx[py][px]
-    print('pivot (x y):', pv)
+    print('pivot:', pv)
     if pv == 0:
         raise Exception('Pivot cannot be zero')
     for i in range(M):
@@ -33,32 +38,33 @@ def step(mtx, pivot):
     return res
 
 
-def read_mtx(string):
-    return [
-        [Fr(n) for n in s.split()]
-        for s in string.split('\n')
-        if s
-    ]
+def read_mtx(fname):
+    ls = open(fname, 'r').readlines()
+    # with open(fname, 'r').readlines() as f:
+    return [[Fr(n) for n in s.split()] for s in ls if s]
 
 
 def print_mtx(mtx, pivot=None):
     print(tabulate(
         [[str(c) for c in row] for row in mtx],
-        tablefmt='fancy_grid',
+        tablefmt='grid',
     ))
 
 
 def write_mtx(fname, mtx):
     open(fname, 'w+').write(
-        '\n'.join(' '.join(str(c) for c in row) for row in mtx)
+        '\n'.join('\t'.join(str(c) for c in row) for row in mtx)
     )
 
 
 if __name__ == '__main__':
-    fname, resname = sys.argv[1:]
-    mtx = read_mtx(open(fname, 'r').read())
-    pivot = [int(s) for s in re.split(r'[, ]+', input('pivot: '))]
+    infile, outfile = sys.argv[1:]
+    mtx = read_mtx(infile)
+    pivot = (
+        int(s)
+        for s in re.split(r'[, ]+', input('pivot postition (x y): '))
+    )
     print_mtx(mtx)
     res = step(mtx, pivot)
     print_mtx(res)
-    write_mtx(resname, res)
+    write_mtx(outfile, res)
